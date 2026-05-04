340939f211802ec62b1ce11eb2ea07ff8925e5f3
MarxLen1n <hugoglezgcia@gmail.com>
2026-05-04 22:01:47 +0100

    Se automatiza la compilación de windows con github así que se elimina esa parte

--- make_windows.sh
+++
@@ -1,7 +0,0 @@
-pyinstaller --onedir ^
---add-binary "ffmpeg/ffmpeg.exe;ffmpeg" ^
---add-binary "ffmpeg/ffprobe.exe;ffmpeg" ^
---add-binary "ffmpeg/ffplay.exe;ffmpeg" ^
---name descargador ^
---icon icon.ico ^
-downloader.py
